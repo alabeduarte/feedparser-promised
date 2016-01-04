2421102668db54e0bce3259fdde5a43d0aa7df18
Linda Nichols <lynnaloo@gmail.com>
2016-01-04 00:51:35 -0500

    Add error-handling
    Adds error handling for request and feedparser.

--- src/feedParserPromised.js
+++ src/feedParserPromised.js
@@ -3,20 +3,23 @@ import FeedParser from 'feedparser';
 
 export default class FeedParserPromised {
   static parse (url) {
-    return new Promise( (resolve) => {
+    return new Promise( (resolve, reject) => {
       const items = [];
       const feedparser = new FeedParser();
 
+      feedparser.on('error', (err) => {
+        reject(err);
+      });
+      
       feedparser.on('readable', () => {
         let item;
-
         while(item = feedparser.read()) { items.push(item); }
-
         return items;
       });
 
       request.get(url)
         .pipe(feedparser)
+        .on('error', (err) => { reject(err); })
         .on('end', () => { return resolve(items); });
     });
   }
