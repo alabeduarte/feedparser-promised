58d1f146bff62b4abb7e6025e9c802c06566cb98
Alabê Duarte <alabeduarte@gmail.com>
2016-07-28 13:34:59 -0300

    Closes #2

--- src/feedParserPromised.js
+++ src/feedParserPromised.js
@@ -2,10 +2,14 @@ import request from 'request';
 import FeedParser from 'feedparser';
 
 export default class FeedParserPromised {
-  static parse (url) {
-    return new Promise( (resolve) => {
+  static parse (uri) {
+    return new Promise( (resolve, reject) => {
       const items = [];
       const feedparser = new FeedParser();
+
+      feedparser.on('error', (err) => {
+        reject(err);
+      });
 
       feedparser.on('readable', () => {
         let item;
@@ -15,7 +19,8 @@ export default class FeedParserPromised {
         return items;
       });
 
-      request.get(url)
+      request.get(uri)
+        .on('error', (err) => { reject(err); })
         .pipe(feedparser)
         .on('end', () => { return resolve(items); });
     });
--- test/feedParserPromised.test.js
+++ test/feedParserPromised.test.js
@@ -9,24 +9,21 @@ describe('FeedparserPromised', () => {
   describe('.parse', () => {
     const aHost = faker.internet.url();
     const aPath = '/rss';
-
     const someUrl = `${aHost}${aPath}`;
+    const rssFile = fs.readFileSync(
+      `${__dirname}/feeds/rss2sample.xml`,
+      'utf-8'
+    )
     const expectedItems = [
       { title: 'Star City' },
       { title: 'The Engine That Does More' },
       { title: 'Astronauts Dirty Laundry' }
     ];
 
-    beforeEach( () => {
-      nock(aHost).get(aPath)
-        .reply(
-          200,
-          fs.readFileSync(`${__dirname}/feeds/rss2sample.xml`, 'utf-8')
-        );
-    });
-
     describe('on success', () => {
       it('parses rss items', (done) => {
+        nock(aHost).get(aPath).reply(200, rssFile);
+
         const promise = FeedParserPromised.parse(someUrl);
 
         promise.then( (items) => {
@@ -44,9 +41,40 @@ describe('FeedparserPromised', () => {
     });
 
     describe('on feedparse error', () => {
-      const invalidUrl = 'invalid url';
+      it('handles error on socket timeout error', (done) => {
+        nock(aHost).get(aPath).socketDelay(2).reply(408, 'ESOCKETTIMEDOUT');
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 1 }
+        );
+
+        promise.catch( (error) => {
+          assert.deepEqual({ code: 'ESOCKETTIMEDOUT', connect: false }, error);
+
+          done();
+        }).catch( (err) => {
+          done(err);
+        });
+      });
+
+      it('handles error on timeout error', (done) => {
+        nock(aHost).get(aPath).delayConnection(2).reply(408, 'ETIMEDOUT');
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 1 }
+        );
+
+        promise.catch( (error) => {
+          assert.deepEqual({ code: 'ETIMEDOUT', connect: false }, error);
+
+          done();
+        }).catch( (err) => {
+          done(err);
+        });
+      });
 
       it('parses rss items', (done) => {
+        const invalidUrl = 'invalid url';
         const promise = FeedParserPromised.parse(invalidUrl);
 
         const errorInvalidURI = new Error('Invalid URI "invalid%20url"');
