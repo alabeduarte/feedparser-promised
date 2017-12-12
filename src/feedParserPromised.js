05d03e46f3dd9d067633360ed54502e643a9a01c
Sergey Drobov <serch.m@gmail.com>
2017-12-12 12:04:30 +0300

    Compressed content and non-utf8 charsets support

--- src/feedParserPromised.js
+++ src/feedParserPromised.js
@@ -2,6 +2,8 @@
 
 const request = require('request');
 const FeedParser = require('feedparser');
+const Iconv = require('iconv').Iconv;
+const zlib = require('zlib');
 
 module.exports = class FeedParserPromised {
   static parse (requestOptions, feedparserOptions) {
@@ -31,8 +33,53 @@ module.exports = class FeedParserPromised {
         req.on('error', (err) => { reject(err); });
       }
 
-      req.pipe(feedparser);
+      req.on('response', (res) => {
+        if (res.statusCode !== 200) {
+          req.emit(new Error('HTTP response code !== 200'));
+        }
+
+        const encoding = res.headers['content-encoding'] || 'identity';
+        const charset = (res.headers['content-type'] || '').split(';').reduce((params, param) => {
+          const parts = param.split('=').map(part => { return part.trim(); });
+
+          if (parts.length === 2) {
+            params[parts[0]] = parts[1];
+          }
+
+          return params;
+        }, {}).charset;
+
+        res = FeedParserPromised._maybeDecompress(res, encoding);
+        res = FeedParserPromised._maybeTranslate(res, charset);
+        res.pipe(feedparser);
+      });
       req.on('end', () => { return resolve(items); });
     });
+  }
+
+  static _maybeDecompress(res, encoding) {
+    let decompress;
+    if (encoding.match(/\bdeflate\b/)) {
+      decompress = zlib.createInflate();
+    } else if (encoding.match(/\bgzip\b/)) {
+      decompress = zlib.createGunzip();
+    }
+    return decompress ? res.pipe(decompress) : res;
+  }
+
+  static _maybeTranslate(res, charset) {
+    let iconv;
+    if (!iconv && charset && !/utf-*8/i.test(charset)) {
+      try {
+        iconv = new Iconv(charset, 'utf-8');
+        iconv.on('error', err => {
+          res.emit('error', err);
+        });
+        res = res.pipe(iconv);
+      } catch (err) {
+        res.emit('error', err);
+      }
+    }
+    return res;
   }
 };
