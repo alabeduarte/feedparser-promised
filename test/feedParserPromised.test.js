507f4e96a9f6f2ee521aeec86b159b1ebc80f815
Sergey Drobov <serch.m@gmail.com>
2018-09-22 00:57:52 +0300

    tests for gzipped and non-utf8 content

--- test/feedParserPromised.test.js
+++ test/feedParserPromised.test.js
@@ -8,7 +8,7 @@ const _ = require('lodash');
 const FeedParserPromised = require('../src/feedParserPromised');
 
 describe('FeedparserPromised', () => {
-  describe('.parse', () => {
+  describe('.parse simple', () => {
     const aHost = faker.internet.url();
     const aPath = '/rss';
     const someUrl = `${aHost}${aPath}`;
@@ -75,6 +75,224 @@ describe('FeedparserPromised', () => {
         promise.catch( (error) => {
           assert.deepEqual(errorInvalidURI, error);
         }).then(done).catch( (err) => done(err));
+      });
+    });
+  });
+
+  describe('.parse cp1251', () => {
+    const aHost = faker.internet.url();
+    const aPath = '/rss';
+    const someUrl = `${aHost}${aPath}`;
+    const rssFile = fs.readFileSync(`${__dirname}/feeds/rss2sample-cp1251.xml`)
+    const expectedItems = [
+      { title: 'Звездный город' },
+      { title: 'The Engine That Does More' },
+      { title: 'Astronauts Dirty Laundry' }
+    ];
+
+    describe('on success', () => {
+      it('parses rss items', (done) => {
+        nock(aHost).get(aPath).reply(200, rssFile, { 'content-type': 'application/rss+xml; charset=windows-1251' });
+
+        const promise = FeedParserPromised.parse(someUrl);
+
+        promise.then((items) => {
+          assert.equal(expectedItems.length, items.length);
+
+          _.zip(expectedItems, items).forEach((zippedItems) => {
+            assert.equal(zippedItems[0].title, zippedItems[1].title);
+          });
+        }).then(done).catch((err) => done(err));
+      });
+
+      it('parses rss items using optionals parameters for request', (done) => {
+        nock(aHost).get(aPath).reply(200, rssFile, { 'content-type': 'application/rss+xml; charset=windows-1251' });
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 3000, foo: 'bar' }
+        );
+
+        promise.then((items) => {
+          assert.equal(expectedItems.length, items.length);
+
+          _.zip(expectedItems, items).forEach((zippedItems) => {
+            assert.equal(zippedItems[0].title, zippedItems[1].title);
+          });
+        }).then(done).catch((err) => done(err));
+      });
+    });
+
+    describe('on feedparse error', () => {
+      it('handles error on socket timeout error', (done) => {
+        nock(aHost).get(aPath).socketDelay(2).reply(408, 'ESOCKETTIMEDOUT');
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 1 }
+        );
+
+        promise.catch((error) => {
+          assert.deepEqual({ code: 'ESOCKETTIMEDOUT', connect: false }, error);
+        }).then(done).catch((err) => done(err));
+      });
+
+      it('handles invalid uri', (done) => {
+        const invalidUri = 'invalid uri';
+        const promise = FeedParserPromised.parse(invalidUri);
+
+        const errorInvalidURI = new Error('Invalid URI "invalid%20uri"');
+        promise.catch((error) => {
+          assert.deepEqual(errorInvalidURI, error);
+        }).then(done).catch((err) => done(err));
+      });
+    });
+  });
+
+  describe('.parse gzipped', () => {
+    const aHost = faker.internet.url();
+    const aPath = '/rss';
+    const someUrl = `${aHost}${aPath}`;
+    const rssFile = fs.readFileSync(`${__dirname}/feeds/rss2sample.xml.gz`)
+    const expectedItems = [
+      { title: 'Star City' },
+      { title: 'The Engine That Does More' },
+      { title: 'Astronauts Dirty Laundry' }
+    ];
+
+    describe('on success', () => {
+      it('parses rss items', (done) => {
+        nock(aHost).get(aPath).reply(200, rssFile, { 'content-encoding': 'gzip' });
+
+        const promise = FeedParserPromised.parse(someUrl);
+
+        promise.then((items) => {
+          assert.equal(expectedItems.length, items.length);
+
+          _.zip(expectedItems, items).forEach((zippedItems) => {
+            assert.equal(zippedItems[0].title, zippedItems[1].title);
+          });
+        }).then(done).catch((err) => done(err));
+      });
+
+      it('parses rss items using optionals parameters for request', (done) => {
+        nock(aHost).get(aPath).reply(200, rssFile, { 'content-encoding': 'gzip' });
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 3000, foo: 'bar' }
+        );
+
+        promise.then((items) => {
+          assert.equal(expectedItems.length, items.length);
+
+          _.zip(expectedItems, items).forEach((zippedItems) => {
+            assert.equal(zippedItems[0].title, zippedItems[1].title);
+          });
+        }).then(done).catch((err) => done(err));
+      });
+    });
+
+    describe('on feedparse error', () => {
+      it('handles error on socket timeout error', (done) => {
+        nock(aHost).get(aPath).socketDelay(2).reply(408, 'ESOCKETTIMEDOUT');
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 1 }
+        );
+
+        promise.catch((error) => {
+          assert.deepEqual({ code: 'ESOCKETTIMEDOUT', connect: false }, error);
+        }).then(done).catch((err) => done(err));
+      });
+
+      it('handles invalid uri', (done) => {
+        const invalidUri = 'invalid uri';
+        const promise = FeedParserPromised.parse(invalidUri);
+
+        const errorInvalidURI = new Error('Invalid URI "invalid%20uri"');
+        promise.catch((error) => {
+          assert.deepEqual(errorInvalidURI, error);
+        }).then(done).catch((err) => done(err));
+      });
+    });
+  });
+
+  describe('.parse cp1251 gzipped', () => {
+    const aHost = faker.internet.url();
+    const aPath = '/rss';
+    const someUrl = `${aHost}${aPath}`;
+    const rssFile = fs.readFileSync(`${__dirname}/feeds/rss2sample-cp1251.xml.gz`)
+    const expectedItems = [
+      { title: 'Звездный город' },
+      { title: 'The Engine That Does More' },
+      { title: 'Astronauts Dirty Laundry' }
+    ];
+
+    describe('on success', () => {
+      it('parses rss items', (done) => {
+        nock(aHost).get(aPath).reply(
+          200,
+          rssFile,
+          {
+            'content-encoding': 'gzip',
+            'content-type': 'application/rss+xml; charset=windows-1251'
+          }
+        );
+
+        const promise = FeedParserPromised.parse(someUrl);
+
+        promise.then((items) => {
+          assert.equal(expectedItems.length, items.length);
+
+          _.zip(expectedItems, items).forEach((zippedItems) => {
+            assert.equal(zippedItems[0].title, zippedItems[1].title);
+          });
+        }).then(done).catch((err) => done(err));
+      });
+
+      it('parses rss items using optionals parameters for request', (done) => {
+        nock(aHost).get(aPath).reply(
+          200,
+          rssFile,
+          {
+            'content-encoding': 'gzip',
+            'content-type': 'application/rss+xml; charset=windows-1251'
+          }
+        );
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 3000, foo: 'bar' }
+        );
+
+        promise.then((items) => {
+          assert.equal(expectedItems.length, items.length);
+
+          _.zip(expectedItems, items).forEach((zippedItems) => {
+            assert.equal(zippedItems[0].title, zippedItems[1].title);
+          });
+        }).then(done).catch((err) => done(err));
+      });
+    });
+
+    describe('on feedparse error', () => {
+      it('handles error on socket timeout error', (done) => {
+        nock(aHost).get(aPath).socketDelay(2).reply(408, 'ESOCKETTIMEDOUT');
+
+        const promise = FeedParserPromised.parse(
+          { uri: someUrl, timeout: 1 }
+        );
+
+        promise.catch((error) => {
+          assert.deepEqual({ code: 'ESOCKETTIMEDOUT', connect: false }, error);
+        }).then(done).catch((err) => done(err));
+      });
+
+      it('handles invalid uri', (done) => {
+        const invalidUri = 'invalid uri';
+        const promise = FeedParserPromised.parse(invalidUri);
+
+        const errorInvalidURI = new Error('Invalid URI "invalid%20uri"');
+        promise.catch((error) => {
+          assert.deepEqual(errorInvalidURI, error);
+        }).then(done).catch((err) => done(err));
       });
     });
   });
